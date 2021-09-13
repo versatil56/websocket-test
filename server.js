86659de6d76b3235c77bfa785d5e9eda172dc559
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2021-09-13 15:14:45 +0100

    test

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var WebSocketServer = require('ws').Server
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
         console.log('Received from client: %s', message);
-        ws.send('Server received from client: ' + message);
+        ws.send({
+            status: "Accepted",
+            currentTime: Date.now(),
+            interval: 0
+        });
     });
  });
