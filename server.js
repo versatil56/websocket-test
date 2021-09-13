6d55d7180c368f969d3ee7c0488d2db3c12b3434
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2021-09-13 14:12:37 +0100

    test

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+const PORT = process.env.PORT || 8010;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 console.log("Server started");
 var Msg = '';
 var WebSocketServer = require('ws').Server
-    , wss = new WebSocketServer({port: 8010});
+    , wss = new WebSocketServer({port: PORT});
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
         console.log('Received from client: %s', message);
