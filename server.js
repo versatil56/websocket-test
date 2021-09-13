1d0f7e33fb55655a78639700bd4ec9e5132f35b1
Juan Antonio Rodríguez Pozo <versatil5634@gmail.com>
2021-09-13 15:41:03 +0100

    test"
    "
    "
    "
    ;

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ var WebSocketServer = require('ws').Server
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
         console.log('Received from client: %s', message);
-        ws.send([JSON.stringify({
+        ws.send(JSON.stringify([{
             status: "Accepted",
             currentTime: "2021-09-13T15:37:08+01:00",
             interval: 1
-        })]);
+        }]));
     });
  });
